d8ef23f65883411c03ea7f549d96641c4eecb528
Фаль Александр Евгеньевич <afal@natcar.ru>
2025-03-27 22:50:04 +0300

    Модуль 8

--- src/app/main.py
+++ src/app/main.py
@@ -1,70 +1,55 @@
-from uuid import uuid4, UUID
-from fastapi import APIRouter, FastAPI
-from dynaconf import Dynaconf
-from dishka.integrations.fastapi import FromDishka, DishkaRoute, setup_dishka
-from pydantic import BaseModel, PrivateAttr
-from app.core.domain.kernel import Location
-from app.core.domain.model.courier.courier import TransportParams, Courier, CourierStatus
-from app.core.domain.model.order.order import Order
-from app.core.domain.services.i_dispatch_service import IDispatchService
-from app.core.ports.i_order_repository import IOrderRepository
+import logging
+from contextlib import asynccontextmanager
+
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.interval import IntervalTrigger
+from dishka.integrations.fastapi import setup_dishka
+from fastapi import FastAPI
+
+from app.adapters.in_.http.apis.couriers_api import courier_router
+from app.adapters.in_.http.apis.orders_api import order_router
+from app.adapters.in_.jobs.jobs import DispatchOrdersJob, MoveCouriersJob
 from app.ioc.composition_root import container
 
-router = APIRouter(route_class=DishkaRoute)
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='[%(asctime)s] %(levelname)s - %(name)s - %(message)s'
+)
 
 
-class TransportDTO(BaseModel):
-    id: UUID
-    name: str
-    speed: int
+def create_scheduler(
+        move_job: MoveCouriersJob,
+        dispatch_job: DispatchOrdersJob,
+) -> AsyncIOScheduler:
+    scheduler = AsyncIOScheduler()
 
-
-class LocationDTO(BaseModel):
-    x: int
-    y: int
-
-
-class CourierDTO(BaseModel):
-    id: UUID
-    name: str
-    transport: TransportDTO
-    location: LocationDTO
-    status: CourierStatus
-
-
-@router.post("/courier:dispatch", tags=["Courier"], )
-async def dispatch_courier(
-        dispatch_service: FromDishka[IDispatchService],
-        order_repository: FromDishka[IOrderRepository],
-) -> CourierDTO | None:
-    transport1 = TransportParams(name="Bike", speed=2)
-    transport2 = TransportParams(name="Car", speed=3)
-
-    courier1 = Courier.create(name="Courier1", transport_params=transport1, location=Location(x=1, y=1))
-    courier2 = Courier.create(name="Courier2", transport_params=transport2, location=Location(x=2, y=1))
-
-    order = await order_repository.get_by_id(uuid4())
-    # Order.create_new(id=uuid4(), location=Location(x=2, y=10)))
-
-    courier_ = await dispatch_service.dispatch(order, [courier1, courier2])
-    return CourierDTO(
-        id=courier_.id
-        , name=courier_.name
-        , transport=TransportDTO(**courier_.transport.model_dump())
-        , location=LocationDTO(**courier_.location.model_dump())
-        , status=courier_.status
+    scheduler.add_job(
+        move_job.run,
+        trigger=IntervalTrigger(seconds=1),
+        name="Move Couriers Every 1s"
     )
+    scheduler.add_job(
+        dispatch_job.run,
+        trigger=IntervalTrigger(seconds=2),
+        name="Dispatch Orders Every 2s"
+    )
+    return scheduler
 
 
-@router.get("/config", tags=["Config"], )
-async def get_config(
-        config: FromDishka[Dynaconf],
-) -> str | None:
-    print(config)
-    print(config.postgres)
-    return ''
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    move_job = await container.get(MoveCouriersJob)
+    dispatch_job = await container.get(DispatchOrdersJob)
+
+    scheduler = create_scheduler(move_job, dispatch_job)
+    scheduler.start()
+
+    yield
+
+    scheduler.shutdown()
 
 
-app = FastAPI()
-app.include_router(router)
+app = FastAPI(lifespan=lifespan)
+app.include_router(order_router)
+app.include_router(courier_router)
 setup_dishka(container, app)
