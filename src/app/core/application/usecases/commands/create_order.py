351fecfb5eb7efacface3d6fa911c076ced5759f
Фаль Александр Евгеньевич <afal@natcar.ru>
2025-03-29 15:56:35 +0300

    Модуль 9

--- src/app/core/application/usecases/commands/create_order.py
+++ src/app/core/application/usecases/commands/create_order.py
@@ -4,7 +4,6 @@ from abc import abstractmethod
 from pydantic import BaseModel
 
 from app.core.application.usecases.common.command import Command, ICommandHandler
-from app.core.domain.kernel import Location
 from app.core.domain.model.order.order import Order
 from app.core.ports.geo_client import IGeoClient
 from app.core.ports.order_repository import IOrderRepository
@@ -27,6 +26,5 @@ class CreateOrderHandler(ICreateOrderHandler):
         self._order_repository = order_repository
 
     async def handle(self, command: CreateOrderCommand) -> None:
-        location_geo = await self._geo_client.get_geolocation("Street")
-        order = Order.create_new(id=command.order_id, location=Location(x=location_geo.x, y=location_geo.y))
+        order = Order.create_new(id=command.order_id, location=await self._geo_client.get_geolocation("Street"))
         await self._order_repository.add(order)
